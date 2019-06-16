73bfd654ba1d283d9e962a3df4df766bbd4bb0a1
Christian Simmons <CSimmonsCS@gmail.com>
2019-06-15 18:57:24 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import AutoCompleteText from './AutoCompleteText.js';
+import countries from './countries';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-Component" >
+          <AutoCompleteText items={countries}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
